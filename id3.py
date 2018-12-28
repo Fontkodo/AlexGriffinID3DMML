7363d2f7ec595fb9ba873aae2017f9d139878034
Fontkodo <alexgriffinsrq@gmail.com>
2018-12-28 01:11:20 +0000

    Update id3.py
    Moved practice data to separate file

--- id3.py
+++ id3.py
@@ -15,29 +15,6 @@ class observation:
     def __repr__(self):
         return "<{},{}>".format(self.target, self.attribs)
 
-'''
-The following is a sample dataset, used for testing the code
-'''
-
-sample = [
-observation('No', { 'Outlook':'Sunny', 'Temp':'Hot', 'Humidity':'High', 'Windy':'False'}),
-observation('No', { 'Outlook':'Sunny', 'Temp':'Hot', 'Humidity':'High', 'Windy':'True'}),
-observation('Yes', { 'Outlook':'Overcast', 'Temp':'Hot', 'Humidity':'High', 'Windy':'False'}),
-observation('Yes', { 'Outlook':'Rainy', 'Temp':'Mild', 'Humidity':'High', 'Windy':'False'}),
-observation('Yes', { 'Outlook':'Rainy', 'Temp':'Cool', 'Humidity':'Normal', 'Windy':'False'}),
-observation('No', { 'Outlook':'Rainy', 'Temp':'Cool', 'Humidity':'Normal', 'Windy':'True'}),
-observation('Yes', { 'Outlook':'Overcast', 'Temp':'Cool', 'Humidity':'Normal', 'Windy':'True'}),
-observation('No', { 'Outlook':'Sunny', 'Temp':'Mild', 'Humidity':'High', 'Windy':'False'}),
-observation('Yes', { 'Outlook':'Sunny', 'Temp':'Cool', 'Humidity':'Normal', 'Windy':'False'}),
-observation('Yes', { 'Outlook':'Rainy', 'Temp':'Mild', 'Humidity':'Normal', 'Windy':'False'}),
-observation('Yes', { 'Outlook':'Sunny', 'Temp':'Mild', 'Humidity':'Normal', 'Windy':'True'}),
-observation('Yes', { 'Outlook':'Overcast', 'Temp':'Mild', 'Humidity':'High', 'Windy':'True'}),
-observation('Yes', { 'Outlook':'Overcast', 'Temp':'Hot', 'Humidity':'Normal', 'Windy':'False'}),
-observation('No', { 'Outlook':'Rainy', 'Temp':'Mild', 'Humidity':'High', 'Windy':'True'})
-]
-
-KEYS = sample[0].attribs.keys()
-
 def extractColumn(obs, key):
     '''
     If one were to imagine the dataset as a table,
